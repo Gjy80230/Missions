6a916f5bb2d6de232ad60a8ce5ca7d747a409874
Gene <gjy80230@foxmail.com>
2021-12-03 22:13:15 +0800

    加载onreadystatechange事件、XML、JSON。

--- Ajax-demo/public/main.js
+++ Ajax-demo/public/main.js
@@ -1,4 +1,29 @@
-console.log('这是main.js')
+getJSON.onclick = ()=>{
+    const request = new XMLHttpRequest()
+    request.open("GET", "/5.json")
+    request.onreadystatechange = ()=>{
+        if(request.readyState === 4 && request.status === 200){
+            console.log(request.response)
+            const object = JSON.parse(request.response)
+            myName.textContent = object.name
+        }
+    }
+    request.send()
+}
+
+getXML.onclick = ()=>{
+    const request = new XMLHttpRequest()
+    request.open("GET", "4.xml")
+    request.onreadystatechange = ()=>{
+        if(request.readyState === 4 && request.status === 200){
+            const dom = request.responseXML
+            const text = dom.getElementsByTagName('warning')[0].textContent
+            console.log(text.trim())
+        }
+        
+    }
+    request.send()
+}
 
 getHTML.onclick = ()=>{
     const request = new XMLHttpRequest()
@@ -28,17 +53,20 @@ getJS.onclick = ()=>{
 }
 getCSS.onclick = ()=>{
     const request = new XMLHttpRequest()
-    request.open("GET", "/style.css")
-    request.onload = ()=>{
-        // console.log('request.response')
-        // console.log(request.response)
-        const style = document.createElement('style')
-        style.innerHTML = request.response
-        document.head.appendChild(style)
-
+    request.open("GET", "/style.css")   //readyState = 1
+    request.onreadystatechange = ()=>{
+        if(request.readyState === 4){
+            if(request.status <= 200 && request.status < 300){
+                console.log('下载完成')
+                // // console.log('request.response')
+                // // console.log(request.response)
+                const style = document.createElement('style')
+                style.innerHTML = request.response
+                document.head.appendChild(style)
+            }else{
+                alert('加载 CSS 失败')
+            }          
+        }
     }
-    request.onerror = ()=>{
-        console.log('失败')
-    }
-    request.send()
+    request.send()  //readyState = 2
 }
