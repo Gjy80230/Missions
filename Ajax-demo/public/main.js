426dc9c6ca5dab624983671771944ce501e9d207
Gene <gjy80230@foxmail.com>
2021-12-03 20:53:24 +0800

    加载JS、HTML

--- Ajax-demo/public/main.js
+++ Ajax-demo/public/main.js
@@ -1,5 +1,31 @@
 console.log('这是main.js')
 
+getHTML.onclick = ()=>{
+    const request = new XMLHttpRequest()
+    request.open("GET", "/3.html")
+    request.onload = ()=>{
+        const div = document.createElement('div')
+        div.innerHTML = request.response
+        document.body.appendChild(div)
+    }
+    request.onerror = ()=>{
+
+    }
+    request.send()
+}
+getJS.onclick = ()=>{
+    const request = new XMLHttpRequest()
+    request.open("GET", "/2.js")
+    request.onload = ()=>{
+        const script = document.createElement('script')
+        script.innerHTML = request.response
+        document.body.appendChild(script)
+    }
+    request.onerror = ()=>{
+
+    }
+    request.send()
+}
 getCSS.onclick = ()=>{
     const request = new XMLHttpRequest()
     request.open("GET", "/style.css")
